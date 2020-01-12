a04e20632dfac179bec18ac8900ae9c130a6569f
aboozar hoseini <aboozarh4@gmail.com>
2020-01-12 06:42:35 +0330

    final change and clear stahs

--- functions.php
+++ functions.php
@@ -39,8 +39,7 @@ function myThemeSetUp()
     add_image_size('main-thumbnials', 200, 133);
 //    add_image_size('main-thumbnials', 130, 75); // smaller
 
-    add_image_size('main-thumbnials', 260, 150);
-//    add_image_size('main-thumbnials2', 130, 75); // smaller
+
 
 }
 
@@ -68,3 +67,21 @@ add_action('wp_enqueue_scripts', 'add_responsive_slider_assets');
 
 
 include_once get_template_directory() . '/inc/custom-post-type.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
